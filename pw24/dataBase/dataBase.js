b124c454b9aa80770dcf0f5af2e5070129d6bb4e
ArtemFedorov <artemfedorov1411@gmail.com>
2026-05-27 14:22:32 +0200

    third fix

--- pw24/dataBase/dataBase.js
+++ pw24/dataBase/dataBase.js
@@ -1,36 +1,21 @@
 export default [
   {
     id: "item-1",
-    img: "https://i.postimg.cc/7Zhqc6KV/Antools.png",
-    url: "https://artem200921.github.io/Antools/",
-    description:
-      "Antools - this is a website with modern tools for developers.",
-  },
-  {
-    id: "item-2",
     img: "https://i.postimg.cc/2SyCJ60X/Bose.png",
     url: "https://artem200921.github.io/Bose_NC_700/",
     description: "Bose - this is a website for learning about Bose NC 700 headphones.",
   },
   {
-    id: "item-3",
+    id: "item-2",
     img: "https://i.postimg.cc/sgL3RrLH/Food-Bootique.png",
     url: "https://artem200921.github.io/FoodBoutique/",
     description: "Food Bootique - this is a website for online food ordering.",
   },
   {
-    id: "item-4",
+    id: "item-3",
     img: "https://i.postimg.cc/htYKqBY3/Sound-Booster.png",
     url: "https://artem200921.github.io/Final_project/",
     description:
       "Sound Booster - this is a website for learning about the nearest musical events around the world.",
-  },
-
-  {
-    id: "item-5",
-    img: "https://i.postimg.cc/N0rBsnqc/Web-Studio.png",
-    url: "https://artem200921.github.io/WebStudio-SCSS/",
-    description:
-      "Web Studio - this is a website for a private company that provides web development services.",
-  },
+  }
 ];
